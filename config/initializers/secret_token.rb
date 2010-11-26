dc8f64ce9486ef207d91f08ae7046c4441306e2d
plusjade <plusjade@gmail.com>
2010-11-25 17:17:59 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wrestling::Application.config.secret_token = '3f73cb070018a63783070bc24e6257623264061db521d551a8d1dd6733489c220726378b6692b8bb2303ccd74c1a62e42bb952607bc5f5f377059925fc5f7ff3'
